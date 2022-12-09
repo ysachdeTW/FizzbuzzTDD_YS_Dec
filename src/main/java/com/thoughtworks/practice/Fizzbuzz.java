14628f54562d0194795ee80e204a6df14207380a
Nathan Buwalda <nathan.buwalda@va.gov>
2022-12-09 10:26:55 -0500

    First commit

---
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -0,0 +1,4 @@
+package com.thoughtworks.practice;
+
+public class Fizzbuzz {
+}
---
+++ src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzbuzzTest {
+
+}
