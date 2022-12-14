746b6730ce3e395c826e6f749eff76f6a73127cd
yashsachde <yash.sachde@thoughtworks.com>
2022-12-13 20:59:02 -0500

    First test for divisible by 3

--- src/main/java/com/thoughtworks/practice/Fizzbuzz.java
+++ src/main/java/com/thoughtworks/practice/Fizzbuzz.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.practice;
 
 public class Fizzbuzz {
+
+    public String generateFizzbuzz(String inputString){
+        StringBuilder response= new StringBuilder();
+         int input =Integer.parseInt(inputString);
+
+         if(input % 3 == 0) response.append("Fizz");
+
+        return response.toString();
+    }
 }
--- src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
+++ src/test/java/com/thoughtworks/practice/FizzbuzzTest.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.practice;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzbuzzTest {
+     @Test
+    public void testFizzBuzzIfDivisibleBy3(){
+        Fizzbuzz fizzbuzz= new Fizzbuzz();
+        String result = fizzbuzz.generateFizzbuzz("3");
+        assertEquals("Fizz",result);
+
+   }
 
 }
